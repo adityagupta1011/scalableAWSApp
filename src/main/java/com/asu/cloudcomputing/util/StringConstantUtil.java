6473fb4e9e1e0c799d43caf5ec20d76409c9f798
Aditya Gupta <aditya.gupta.1011@gmail.com>
2019-03-01 15:49:46 -0700

    first commit

---
+++ src/main/java/com/asu/cloudcomputing/util/StringConstantUtil.java
@@ -0,0 +1,24 @@
+package com.asu.cloudcomputing.util;
+
+public class StringConstantUtil {
+	
+ public static String APP_NAME = "/cloudimagerecognition.php";
+ public static String RQ_QUEUE = "requestQueue";
+ public static String RS_QUEUE = "responseQueue";
+ public static String REGION = "us-west-1";
+ public static String INSTANCE_TYPE = "t2.micro";
+ public static String APPSVR_CLASS = "com.asu.cloudcomputing.imagerecognition.AppTier";
+ public static String MAX_LONG_POLL = "10";
+ 
+ public static String ACCESS_KEY = "";
+ public static String ACCESS_KEY_ID = "";
+ public static String PUBLIC_KEY_NAME = "";
+ public static String BUCKET_NAME = "";
+ public static String AMI = "";
+
+ public static Integer HTTP_PORT = 8888;
+ public static Integer RES_THREAD_COUNT = 4;
+ public static Integer REQ_THREAD_COUNT = 1;
+ public static Integer MAX_INSTANCE_COUNT = 19;
+ 
+}
